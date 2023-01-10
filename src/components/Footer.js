1d395f3304c83914eb15e0752a4c54d2231d3cbc
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-01-10 16:36:57 +0200

    complete project

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import { TiSocialPinterest } from "react-icons/ti";
 
 const Footer = () => {
   return (
-    <div className="w-full bg-gray-900 text-gray-300 py-11 px-5 mt-24 lg:grid lg:grid-cols-2 lg:px-28 lg:pb-0">
-      <div className="grid grid-cols-2 lg:grid-cols-4 lg:w-[55vw]">
+    <div className="w-full bg-gray-900 text-gray-300 py-11 sm:text-center px-5 mt-24 md:grid md:grid-cols-2 md:pb-0 md:px-0 lg:px-1">
+      <div className="grid grid-cols-2 md:grid-cols-4 md:w-[60vw] md:p-0 ">
         <div className="p-3">
           <h3 className="uppercase font-bold ">Solutions</h3>
           <p className="py-1">Travel</p>
@@ -38,7 +38,7 @@ const Footer = () => {
           <p className="py-1">Conditions</p>
         </div>
       </div>
-      <div className="px-2 py-6 flex flex-col w-full h-full lg:pt-1 lg:ml-auto lg:max-w-[500px] lg:p-10 ">
+      <div className="px-2 py-6 flex flex-col w-full h-full md:pt-1 md:ml-auto md:max-w-[300px] md:p-5 lg:max-w-[400px] lg:px-10 lg:mx-auto">
         <div>
           <h3 className="font-bold my-2 pb-2 ">
             SUBSCRIBE TO YOUR NEWSLETTERS
@@ -50,16 +50,16 @@ const Footer = () => {
         </div>
 
         {/* form for newsletter */}
-        <div className="py-2 lg:flex lg:items-center lg:gap-4 ">
+        <div className="py-2 md:flex md:items-center md:gap-4 ">
           <form>
             <input
-              className="w-full my-4 p-2 rounded-md lg:w-[350px]"
+              className="w-full my-4 p-2 rounded-md lg:w-[250px]"
               type="text"
               placeholder="Enter Email.."
             />
           </form>
           <div>
-            <button className="p-2 w-full rounded-md lg:max-w-[90px]">
+            <button className="p-2 w-full rounded-md md:max-w-[90px]">
               Subscribe
             </button>
           </div>
@@ -67,15 +67,15 @@ const Footer = () => {
       </div>
 
       {/* Social media */}
-      <div className="flex flex-col text-center mt-10 text-gray-500 border-t-2 border-gray-500 lg:flex-row lg:items-center lg:justify-between lg:w-screen lg:max-w-[85vw] lg:mb-0">
-        <p className="p-2 text-lg my-5 ">
+      <div className="flex flex-col text-center mt-10 text-gray-500 border-t-2 border-gray-500 md:flex-row md:items-center md:justify-between md:w-screen md:max-w-[90vw] md:mb-0">
+        <p className="p-2 text-md my-5 ">
           2022 Experiences, LLC. All rights reserved
         </p>
         <div className="flex justify-between text-2xl mt-2">
-          <FaFacebook className="lg:mx-10" />
-          <FaInstagram className="lg:mx-10" />
-          <FaTwitter className="lg:mx-10" />
-          <TiSocialPinterest className="lg:mx-10" size={35} />
+          <FaFacebook className="md:mx-5" />
+          <FaInstagram className="md:mx-5" />
+          <FaTwitter className="md:mx-5" />
+          <TiSocialPinterest className="md:mx-5" size={35} />
         </div>
       </div>
     </div>
